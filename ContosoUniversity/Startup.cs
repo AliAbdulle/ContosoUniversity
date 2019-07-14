17571f649660cc8128a4745bb328f41017121e62
AliAbdulle <alidhobey@gmail.com>
2019-07-14 18:48:07 -0500

    Adde new Model entity

--- ContosoUniversity/Startup.cs
+++ ContosoUniversity/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContosoUniversity.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -33,8 +34,11 @@ namespace ContosoUniversity
             });
 
 
+
             //services.AddDbContext<SchoolContext>(options =>
             // options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<SchoolContext>(options =>
+             options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddMvc();
         }
