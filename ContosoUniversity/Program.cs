d13466d09275f78062754fc10ae625d3c0a03bea
AliAbdulle <alidhobey@gmail.com>
2019-07-14 21:00:56 -0500

    Saved all the work

--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using ContosoUniversity.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -19,7 +20,17 @@ namespace ContosoUniversity
 
             using (var scope = host.Services.CreateScope())
             {
-
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<SchoolContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
             }
 
             host.Run();
